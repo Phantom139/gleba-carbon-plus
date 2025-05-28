dd1d6ec75f47ff25341ee2991a188cae99d3c5e5
Robert Fritzen <R-Fritzen@wiu.edu>
2025-05-28 17:43:52 -0500

    Lignumis Compat
    Added an adjustment to the wood processing recipe update to allow mod compatibility with Lignumis. (This was included in the 1.0.2 release on the mod portal)

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,12 @@
+local pressureMin = 1000
+local pressureMax = 2000
+
+if mods["lignumis"] then
+	pressureMin = 900
+end
+
 -- Update tree-plant to allow for planting on Gleba on the new tile
-data.raw["plant"]["tree-plant"].surface_conditions = { {property = "pressure", min = 1000, max = 2000}}
+data.raw["plant"]["tree-plant"].surface_conditions = { {property = "pressure", min = pressureMin, max = pressureMax}}
 table.insert(data.raw["plant"]["tree-plant"].autoplace.tile_restriction, "gleba-artificial-tree-soil-tile")
 -- Update the wood processing recipe to be allowed on Gleba
-data.raw["recipe"]["wood-processing"].surface_conditions = { {property = "pressure", min = 1000, max = 2000}}
+data.raw["recipe"]["wood-processing"].surface_conditions = { {property = "pressure", min = pressureMin, max = pressureMax}}
