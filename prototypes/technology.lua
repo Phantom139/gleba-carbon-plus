361eb9802062a44e1a07850cec86a8d67a8d1722
Robert Fritzen <R-Fritzen@wiu.edu>
2025-11-20 21:03:15 -0600

    1.0.7
    KR2SO Compatibility

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,66 +1,128 @@
-data:extend(
-  {
-    {
-      type = "technology",
-      name = "gleba-tree-seeding",
-      icon = "__gleba-carbon-plus__/graphics/technology/gleba-tree-seeding.png",
-      icon_size = 256,
-      prerequisites = { "tree-seeding", "biter-egg-handling" },
-      effects =
-      {
-        {
-          type = "unlock-recipe",
-          recipe = "gleba-artificial-tree-soil"
-        }
-      },
-      unit =
-      {
-        count = 2500,
-        ingredients =
-        {
-          { "automation-science-pack",      1 },
-          { "logistic-science-pack",        1 },
-          { "chemical-science-pack",        1 },
-          { "production-science-pack",      1 },
-          { "utility-science-pack",         1 },
-          { "space-science-pack",           1 },
-          { "agricultural-science-pack",    1 }
-        },
-        time = 30
-      }
-	},
-    {
-      type = "technology",
-      name = "biocoal",
-      icon = "__gleba-carbon-plus__/graphics/technology/biocoal.png",
-      icon_size = 256,
-      prerequisites = { "advanced-asteroid-processing", "rocket-turret", "gleba-tree-seeding" },
-      effects =
-      {
-        {
-          type = "unlock-recipe",
-          recipe = "biocoal"
-        }
-      },
-      unit =
-      {
-        count = 5000,
-        ingredients =
-        {
-          { "automation-science-pack",      1 },
-          { "logistic-science-pack",        1 },
-          { "chemical-science-pack",        1 },
-          { "production-science-pack",      1 },
-          { "utility-science-pack",         1 },
-          { "space-science-pack",           1 },
-          { "agricultural-science-pack",    1 }
-        },
-        time = 30
-      }
-	},	
-})
+if mods["Krastorio2-spaced-out"] then
+	data:extend(
+	  {
+		{
+		  type = "technology",
+		  name = "gleba-tree-seeding",
+		  icon = "__gleba-carbon-plus__/graphics/technology/gleba-tree-seeding.png",
+		  icon_size = 256,
+		  prerequisites = { "kr-gleba-greenhouse", "biter-egg-handling" },
+		  effects =
+		  {
+			{
+			  type = "unlock-recipe",
+			  recipe = "gleba-artificial-tree-soil"
+			}
+		  },
+		  unit =
+		  {
+			count = 2500,
+			ingredients =
+			{
+				{ "production-science-pack", 1 },
+				{ "utility-science-pack", 1 },
+				{ "space-science-pack", 1 },
+				{ "kr-matter-tech-card", 1 },
+				{ "kr-advanced-tech-card", 1 },
+				{ "agricultural-science-pack", 1 }
+			},
+			time = 30
+		  }
+		},	
+		{
+		  type = "technology",
+		  name = "biocoal",
+		  icon = "__gleba-carbon-plus__/graphics/technology/biocoal.png",
+		  icon_size = 256,
+		  prerequisites = { "advanced-asteroid-processing", "rocket-turret", "gleba-tree-seeding" },
+		  effects =
+		  {
+			{
+			  type = "unlock-recipe",
+			  recipe = "biocoal"
+			}
+		  },
+		  unit =
+		  {
+			count = 5000,
+			ingredients =
+			{
+				{ "production-science-pack", 1 },
+				{ "utility-science-pack", 1 },
+				{ "space-science-pack", 1 },
+				{ "kr-matter-tech-card", 1 },
+				{ "kr-advanced-tech-card", 1 },
+				{ "agricultural-science-pack", 1 }
+			},
+			time = 30
+		  }
+		},			
+	})
+else
+	data:extend(
+	  {
+		{
+		  type = "technology",
+		  name = "gleba-tree-seeding",
+		  icon = "__gleba-carbon-plus__/graphics/technology/gleba-tree-seeding.png",
+		  icon_size = 256,
+		  prerequisites = { "tree-seeding", "biter-egg-handling" },
+		  effects =
+		  {
+			{
+			  type = "unlock-recipe",
+			  recipe = "gleba-artificial-tree-soil"
+			}
+		  },
+		  unit =
+		  {
+			count = 2500,
+			ingredients =
+			{
+			  { "automation-science-pack",      1 },
+			  { "logistic-science-pack",        1 },
+			  { "chemical-science-pack",        1 },
+			  { "production-science-pack",      1 },
+			  { "utility-science-pack",         1 },
+			  { "space-science-pack",           1 },
+			  { "agricultural-science-pack",    1 }
+			},
+			time = 30
+		  }
+		},
+		{
+		  type = "technology",
+		  name = "biocoal",
+		  icon = "__gleba-carbon-plus__/graphics/technology/biocoal.png",
+		  icon_size = 256,
+		  prerequisites = { "advanced-asteroid-processing", "rocket-turret", "gleba-tree-seeding" },
+		  effects =
+		  {
+			{
+			  type = "unlock-recipe",
+			  recipe = "biocoal"
+			}
+		  },
+		  unit =
+		  {
+			count = 5000,
+			ingredients =
+			{
+			  { "automation-science-pack",      1 },
+			  { "logistic-science-pack",        1 },
+			  { "chemical-science-pack",        1 },
+			  { "production-science-pack",      1 },
+			  { "utility-science-pack",         1 },
+			  { "space-science-pack",           1 },
+			  { "agricultural-science-pack",    1 }
+			},
+			time = 30
+		  }
+		},			
+	})	
+end
 
-if settings.startup["enable-fruit-productivity"].value then
+if settings.startup["enable-fruit-productivity"].value and not mods["Krastorio2-spaced-out"] then
 	local cost_scaling = settings.startup["fruit-productivity-cost-scaling"].value
 	local cost_formula = string.format("(%s)^(L-1)*1000", cost_scaling)
 
